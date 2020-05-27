bd16e458a92f48f3dbb57a93de6cd57db2edf484
milos-andric <55629183+milos-andric@users.noreply.github.com>
2020-05-27 09:41:42 +0200

    fix

--- ex05/eval.py
+++ ex05/eval.py
@@ -1,21 +1,19 @@
-def zip_evaluate(coefs, words):
-	if len(coefs) != len(words):
-		return (-1)
-	mapped = zip(coefs, words)
-	somme = 0
-	for words in mapped:
-		somme += words[0] * len(words[1])
+
+class Evaluator:
+	def zip_evaluate(coefs, words):
+		if len(coefs) != len(words):
+			return(-1)
+		mapped = zip(coefs, words)
+		somme = 0
+		for words in mapped:
+			somme += words[0] * len(words[1])
+		return(somme)
 
 
-def enumerate_evaluate(coefs, words):
-	if len(coefs) != len(words):
-		return (-1)
-	somme = 0
-	for i, j in enumerate(words):
-		somme += len(j) * coefs[i]
-
-
-words = ["Le", "Lorem", "Ipsum", "est", "simple"]
-coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
-
-enumerate_evaluate(coefs, words)
+	def enumerate_evaluate(coefs, words):
+		if len(coefs) != len(words):
+			return(-1)
+		somme = 0
+		for i, j in enumerate(words):
+			somme += len(j) * coefs[i]
+		return(somme)
